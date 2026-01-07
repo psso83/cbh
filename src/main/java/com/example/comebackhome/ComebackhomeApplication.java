c8f4510c5affd38cdf149ac87ea146c035f6060c
psso83 <pkse11283@g.hongik.ac.kr>
2026-01-07 18:08:51 +0900

    feat : hi추가

---
+++ src/main/java/com/example/comebackhome/ComebackhomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.comebackhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComebackhomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComebackhomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/comebackhome/ComebackhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comebackhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComebackhomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
